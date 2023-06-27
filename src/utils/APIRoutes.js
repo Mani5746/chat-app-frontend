a4da1658ae2923c9dbfb123454600480f4b723d2
Manish Srivastav <84321476+Mani5746@users.noreply.github.com>
2023-06-27 20:29:13 +0530

    Update APIRoutes.js

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host="chat-app-backend-production-f896.up.railway.app";
+export const host="https://chat-app-backend-production-f896.up.railway.app/";
 export const registerRoute=`${host}/api/auth/register`;
 export const loginRoute=`${host}/api/auth/login`;
 export const setAvatarRoute=`${host}/api/auth/setAvatar`;
